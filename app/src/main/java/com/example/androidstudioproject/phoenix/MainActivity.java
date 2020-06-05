1d9ae3d04a5804dde36d1bfa527f3ae878b3f98a
Prithwi <dasprithwibitas@gmail.com>
2020-06-06 00:41:39 +0530

    WebView Application

---
+++ app/src/main/java/com/example/androidstudioproject/phoenix/MainActivity.java
@@ -0,0 +1,170 @@
+package com.example.androidstudioproject.phoenix;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.net.http.SslError;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    String ShowOrHideWebViewInitialUse = "show";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.webview);
+
+        webView.setWebViewClient(new myWebClient());
+            WebSettings webSettings = webView.getSettings();
+            webSettings.setJavaScriptEnabled(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setAllowContentAccess(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+
+        webView.getSettings().setPluginState(WebSettings.PluginState.ON);
+        webView.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
+
+        webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+
+
+        webSettings.setAllowFileAccessFromFileURLs(true);
+        webSettings.setAllowUniversalAccessFromFileURLs(true);
+        webSettings.setDomStorageEnabled(true);
+        if (Build.VERSION.SDK_INT >= 19) {
+            webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        }
+        else {
+            webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        }
+
+        webView.setWebViewClient(new WebViewClient(){
+
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                // DO NOT CALL SUPER METHOD
+                super.onReceivedSslError(view, handler, error);
+            }
+
+        });
+        webView.loadUrl("https://avenir2k19.herokuapp.com");
+
+
+            webView.setWebViewClient(new WebViewClient(){
+                @Override
+                public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                    webView.loadUrl("file:///android_asset/html.html");
+
+                }
+            });
+
+
+
+
+
+    }
+
+
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+                    if (webView.canGoBack()) {
+                        webView.goBack();
+                    } else {
+                        finish();
+                    }
+                    return true;
+            }
+
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class myWebClient extends WebViewClient {
+        @Override
+        public void onPageStarted(WebView webView, String url, Bitmap favicon) {
+            if (ShowOrHideWebViewInitialUse.equals("show")) {
+                webView.setVisibility(webView.INVISIBLE);
+            }
+            super.onPageStarted(webView, url, favicon);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+
+
+            ImageView imgLoading = (ImageView)findViewById(R.id.LoadingImage);
+            imgLoading.setVisibility(imgLoading.GONE);
+
+            view.setVisibility(webView.VISIBLE);
+            super.onPageFinished(view, url);
+
+        }
+
+
+
+//        @Override
+//        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+//
+//            if(Uri.parse(url).getHost().endsWith("avenir2k19.herokuapp.com")) {
+//                return false;
+//            }
+//
+//            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+//            view.getContext().startActivity(intent);
+//            return true;
+//        }
+
+
+    }
+
+
+}
